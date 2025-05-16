c2dd099925555f3c0dd71a03168af14f6c8d919c
vishal-dk <vishaldk758@gmail.com>
2025-05-16 11:38:24 +0530

    first comment

---
+++ dk.js
@@ -0,0 +1,50 @@
+/*const hi=(callback) =>{
+   setTimeout(()=> {
+    console.log("Hi")
+},1000)
+callback() 
+    }
+const bye=() =>{
+    console.log("bye")
+}
+hi(bye);*/
+
+setTimeout (() =>{
+    document.getElementById("count").innerHTML=10
+    setTimeout(() =>{
+        document.getElementById("count").innerHTML=9
+        setTimeout(() =>{
+            document.getElementById("count").innerHTML=8
+            setTimeout (() =>{
+    document.getElementById("count").innerHTML=7
+    setTimeout(() =>{
+        document.getElementById("count").innerHTML=6
+        setTimeout(() =>{
+            document.getElementById("count").innerHTML=5
+setTimeout (() =>{
+    document.getElementById("count").innerHTML=4
+    setTimeout(() =>{
+        document.getElementById("count").innerHTML=3
+        setTimeout(() =>{
+            document.getElementById("count").innerHTML=2
+                 setTimeout(() =>{
+            document.getElementById("count").innerHTML=1
+            setTimeout(() =>{
+             document.getElementById("count").innerHTML="dk_vishal_24"
+    
+            
+           },1000)
+
+        },1000)
+    },1000)
+},1000)
+ },1000)
+
+        },1000)
+    },1000)
+},1000)
+ },1000)
+},1000)
+},1000)
+
+
